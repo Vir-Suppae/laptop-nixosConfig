d1f7b01b3ff6d11135d8df2dc6208d04658fd1c9
vir-suppae <ezekielv09@outlook.com>
2026-07-06 14:46:00 -0500

    enabled bluetooth

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+# Edt this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -33,6 +33,9 @@
     extraPackages = with pkgs; [
       intel-media-driver
     ];
+  };
+  hardware.bluetooth = {
+    enable = true;
   };
 
   # Set your time zone.
